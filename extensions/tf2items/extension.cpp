d627f523d5d048986e0a2d7a097b8447001d51c4
szentigrad3 <nofukn@gmail.com>
2026-03-18 02:06:59 +1100

    Delete extensions/tf2items directory

--- extensions/tf2items/extension.cpp
+++
@@ -1,8 +0,0 @@
-
-// === TF2C CORE REWRITE HOOK ===
-#include "index_map.h"
-
-void OnItemCreated(void* item, int defIndex)
-{
-    ApplyOverrides(defIndex, item);
-}
--- extensions/tf2items/index_map.cpp
+++
@@ -1,3 +0,0 @@
-#include "index_map.h"
-
-std::unordered_map<int, ItemOverride> g_ItemOverrides;
